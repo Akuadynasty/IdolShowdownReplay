11c3e271403e44cff877ec7229e04a0aac93a368
Tangethan39 <72875233+Tangethan39@users.noreply.github.com>
2024-03-05 17:57:46 -0500

    upload replay site to github

---
+++ server/charIdMap.js
@@ -0,0 +1,62 @@
+module.exports = {
+	"ak": {
+		"id": "ak",
+		"name": "Aki",
+		"full": "Aki Rosenthal",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Aki_Rosenthal"
+	},
+	"ay": {
+		"id": "ay",
+		"name": "Ayame",
+		"full": "Ayame Nakiri",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Ayame_Nakiri"
+	},
+	"bo": {
+		"id": "bo",
+		"name": "Botan",
+		"full": "Botan Shishiro",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Botan_Shishiro"
+	},
+	"co": {
+		"id": "co",
+		"name": "Coco",
+		"full": "Coco Kiryu",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Coco_Kiryu"
+	},
+	"fu": {
+		"id": "fu",
+		"name": "Fubuki",
+		"full": "Fubuki Shirakami",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Fubuki_Shirakami"
+	},
+	"ko": {
+		"id": "ko",
+		"name": "Korone",
+		"full": "Korone Inugami",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Korone_Inugami"
+	},
+	"ol": {
+		"id": "ol",
+		"name": "Ollie",
+		"full": "Ollie Kureji",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Kureji_Ollie"
+	},
+	"so": {
+		"id": "so",
+		"name": "Sora",
+		"full": "Sora Tokino",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Sora_Tokino"
+	},
+	"su": {
+		"id": "su",
+		"name": "Suisei",
+		"full": "Suisei Hoshimachi",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Suisei_Hoshimachi"
+	},
+	"pe": {
+		"id": "pe",
+		"name": "Pekora",
+		"full": "Pekora Usada",
+		"wiki": "https://wiki.gbl.gg/w/Idol_Showdown/Pekora_Usada"
+	}
+};
