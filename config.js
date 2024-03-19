d11a75a7c478b02a83d60d70ea7b3b0247c63558
Tangethan39 <72875233+Tangethan39@users.noreply.github.com>
2024-03-19 17:57:44 -0400

    removed config.js from gitignore

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	"port": 8888,
+	"matchData": "data/match.data",
+	"reportsFilename": "data/reports.data",
+	"logFilename": "data/log-DATE.data",
+	"resultsPerPage": 50,
+	"server": "./server/index.js",
+	"contributors": [
+		{
+			"name": "ViolinGolfer",
+			"html": "<p>Keeper of the replays</p><a href='https://twitter.com/thegame012345a'></a>",
+			"img": "/img/VG.png",
+			"token": "VG"
+		}
+	]
+};
