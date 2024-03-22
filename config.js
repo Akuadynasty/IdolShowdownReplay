90c68228efebd94a1b2906513097054b9cb5a92e
Tangethan39 <72875233+Tangethan39@users.noreply.github.com>
2024-03-22 16:31:04 -0400

    contributor update

--- config.js
+++ config.js
@@ -11,6 +11,42 @@ module.exports = {
 			"html": "<p>Keeper of the replays</p><a href='https://twitter.com/thegame012345a'></a>",
 			"img": "/img/VG.png",
 			"token": "VG"
+		},
+		{
+			"name": "Edward Yolag",
+			"html": "<p>Programador Web, videojuegos, Youtube, dormir.</p><a href=></a>",
+			"img": "/img/EY.png",
+			"token": "EY"
+		},
+		{
+			"name": "Akuadynasty",
+			"html": "<p>Programmer and fighting game player. Ayame bros</p><a href=></a>",
+			"img": "/img/AK.png",
+			"token": "AK"
+		},
+		{
+			"name": "Chzchan",
+			"html": "<p>Idolshowdown community hero</p><a href=''></a>",
+			"img": "/img/CHZ.png",
+			"token": "CHZ"
+		},
+		{
+			"name": "Gigadrill",
+			"html": "<p>North America's retired Fubuki</p><a href=''></a>",
+			"img": "/img/GD.png",
+			"token": "GD"
+		},
+		{
+			"name": "Lozardo",
+			"html": "<p>Local NA/OCE fubuki player from EU</p><a href=''></a>",
+			"img": "/img/LZ.png",
+			"token": "LZ"
+		},
+		{
+			"name": "Crackin Atkins",
+			"html": "<p>Face of the Idolshowdown community</p><a href=''></a>",
+			"img": "/img/CA.png",
+			"token": "CA"
 		}
 	]
 };
